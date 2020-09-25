4c100bbe2dcd3f1d37fa889abbdd79b56d3b9995
tindvor239 <37416837+tindvor239@users.noreply.github.com>
2020-09-25 21:20:52 +0700

    Add codes and stuffs

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TileManager))]
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TileManager tileManager;
+    #region Singleton
+    public static GameManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+    #region Properties
+    public TileManager TileManager { get => tileManager; }
+    #endregion
+    // Start is called before the first frame update
+    private void Start()
+    {
+        tileManager = GetComponent<TileManager>();
+        tileManager.GetTileBlocks();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        tileManager.GetTileOnClick();
+    }
+}
---
+++ Assets/Script/TileManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileManager : MonoBehaviour
+{
+    #region Field
+    [SerializeField] private Tilemap map;
+    [SerializeField] private Grid grid;
+    [SerializeField] private TileBase[,] blocks;
+    #endregion
+    #region Behavior
+    public void GetTileBlocks()
+    {
+        BoundsInt bounds = map.cellBounds;
+        ushort length = (ushort)Mathf.Sqrt(GetLenght(bounds));
+        Test(length, "length");
+        blocks = new TileBase[length, length];
+        ushort index = 0;
+        for (sbyte row = 0; row < length; row++)
+        {
+            for (sbyte column = 0; column < length; column++)
+            {
+                if(map.GetTilesBlock(bounds)[index] != null)
+                {
+                    blocks[row, column] = map.GetTilesBlock(bounds)[index];
+                    index++;
+                }
+            }
+        }
+    }
+    private ushort GetLenght(BoundsInt bounds)
+    {
+        ushort count = 0;
+        foreach (TileBase tile in map.GetTilesBlock(bounds))
+        {
+            if (tile != null)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    private void Test(ushort paragramValue, string paragramName)
+    {
+        if(Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            Debug.Log(string.Format("{1}: {0}", paragramValue, paragramName));
+        }
+    }
+    public void GetTileOnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3Int mousePos = new Vector3Int ((int)Input.mousePosition.x, (int)Input.mousePosition.y, (int)Input.mousePosition.z);
+            Debug.Log(string.Format("mouse to tilemap position: {0}",grid.GetCellCenterWorld(mousePos)));
+        }
+    }
+    #endregion
+}
